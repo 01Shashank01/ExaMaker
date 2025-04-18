a344e69002d6acedcaa53612a8d8d02349ffda9b
Shashank Jain <159939097+01Shashank01@users.noreply.github.com>
2025-04-18 15:55:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from model import generate_question_paper
+from xhtml2pdf import pisa
+from io import BytesIO
+import PyPDF2
 
 app = Flask(__name__)
+
+def extract_text_from_pdf(pdf_file):
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    extracted_text = ""
+    for page in pdf_reader.pages:
+        extracted_text += page.extract_text() or ""
+    return extracted_text
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        context = request.form.get('context', '')
+        # Check if PDF file was uploaded
+        if 'pdf_file' in request.files and request.files['pdf_file'].filename != '':
+            pdf_file = request.files['pdf_file']
+            context = extract_text_from_pdf(pdf_file)
+        else:
+            context = request.form.get('context', '')
 
         # Collect user-defined question requirements from form
         question_requirements = {
@@ -33,10 +48,23 @@ def home():
         }
 
         question_paper = generate_question_paper(context, question_requirements)
-
         return render_template('index.html', context=context, question_paper=question_paper)
 
     return render_template('index.html')
 
+@app.route('/download-pdf', methods=['POST'])
+def download_pdf():
+    question_paper = request.form.get('pdf_content', '')
+    pdf_io = BytesIO()
+    html_content = f"<html><body><pre>{question_paper}</pre></body></html>"
+    pisa_status = pisa.CreatePDF(html_content, dest=pdf_io)
+
+    if pisa_status.err:
+        return "Error while generating PDF", 500
+
+    pdf_io.seek(0)
+    return send_file(pdf_io, download_name="question_paper.pdf", as_attachment=True)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
